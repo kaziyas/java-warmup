5e55ebbb83ba0310a21a86f2bbe9aa92979da158
Yaser Kazerooni <yaser.kazerooni@outlook.com>
2023-04-01 20:23:12 +0200

    implement GrowingAndShrinkingPotions code challenge

--- src/main/java/com/edabit/GrowingShrinkingPotions.java
+++ src/main/java/com/edabit/GrowingShrinkingPotions.java
@@ -1,8 +1,5 @@
 package com.edabit;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 /**
  * @author yaser.kazerooni (yaser.kazerooni@gmail.com)
  * @created 01/April/2023
@@ -16,16 +13,16 @@ public class GrowingShrinkingPotions {
             switch (array[i]) {
                 case "A": {
                     result[i] = "";
-                    result[i - 1] = "" + (Integer.parseInt(result[i - 1]) + 1);
+                    result[i - 1] = String.valueOf(Integer.parseInt(result[i - 1]) + 1);
                     break;
                 }
                 case "B": {
                     result[i] = "";
-                    result[i - 1] = "" + (Integer.parseInt(result[i - 1]) - 1);
+                    result[i - 1] = String.valueOf(Integer.parseInt(result[i - 1]) - 1);
                     break;
                 }
             }
         }
-        return Arrays.stream(result).collect(Collectors.joining());
+        return String.join("", result);
     }
 }
