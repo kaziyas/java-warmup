278f8f9f126eae48e08e0523c2dbcab67dffc44b
Yaser Kazerooni <yaser.kazerooni@outlook.com>
2023-04-03 22:43:44 +0200

    implement SelectLetters code challenge

--- src/main/java/com/edabit/SelectLetters.java
+++ src/main/java/com/edabit/SelectLetters.java
@@ -10,16 +10,14 @@ import java.util.stream.IntStream;
  */
 public class SelectLetters {
 
-
     public static String solution(String s1, String s2) {
-        // selectLetters("EVERYTHING", "SomeThings") ➞  "EYSomeThings"
-        int length = s1.length() > s2.length() ? s2.length() : s1.length();
+        int length = Math.min(s1.length(), s2.length());
         String result = IntStream.range(0, length)
                 .mapToObj(i -> Character.isUpperCase(s2.charAt(i)) ? s1.charAt(i) : "")
-                .map(e -> String.valueOf(e)).collect(Collectors.joining());
+                .map(String::valueOf).collect(Collectors.joining());
         String result1 = IntStream.range(0, length)
                 .mapToObj(i -> Character.isUpperCase(s1.charAt(i)) ? s2.charAt(i) : "")
-                .map(e -> String.valueOf(e)).collect(Collectors.joining());
+                .map(String::valueOf).collect(Collectors.joining());
         return String.format("%s%s", result, result1);
     }
 }
