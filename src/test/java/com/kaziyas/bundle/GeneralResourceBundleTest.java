505f542bdfff831b00af2f8fb753ef141c426d0d
Yaser Kazerooni <yaser.kazerooni@outlook.com>
2023-04-05 13:12:26 +0200

    Add a test unit to test reading from the resource bundle

---
+++ src/test/java/com/kaziyas/bundle/GeneralResourceBundleTest.java
@@ -0,0 +1,34 @@
+package com.kaziyas.bundle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author yaser.kazerooni (yaser.kazerooni@gmail.com)
+ * @created 05/April/2023
+ * @project java-warmup
+ */
+public class GeneralResourceBundleTest {
+    private GeneralResourceBundle resourceBundle;
+    @Before
+    public void setup() {
+        List<String> bundles = Collections.unmodifiableList(Arrays.asList("com.kaziyas.bundle.account", "com.kaziyas.bundle.shipping"));
+        resourceBundle = new GeneralResourceBundle(bundles);
+    }
+    @Test
+    public void handleGetObject() {
+        assertEquals("Please enter a valid password.", resourceBundle.handleGetObject("account.update_password.new_password.error.required"));
+    }
+
+    @Test
+    public void getKeys() {
+        long size = Collections.list(resourceBundle.getKeys()).size();
+        assertEquals(17, size);
+    }
+}
